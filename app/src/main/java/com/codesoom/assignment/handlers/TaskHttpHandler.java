45e2275159713e850b514382d900992533a02dd6
pangnem <sprnd645@gmail.com>
2021-09-23 13:27:29 +0900

    feat: 메서드에별 http 응답 코드 설정

--- app/src/main/java/com/codesoom/assignment/handlers/TaskHttpHandler.java
+++ app/src/main/java/com/codesoom/assignment/handlers/TaskHttpHandler.java
@@ -4,23 +4,52 @@ import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 import java.io.IOException;
 import java.io.OutputStream;
+import java.util.regex.Pattern;
 
 public class TaskHttpHandler implements HttpHandler {
 
     @Override
     public void handle(HttpExchange httpExchange) throws IOException {
-        int httpStatusCode = 200;
+        String method = httpExchange.getRequestMethod();
+        String path = httpExchange.getRequestURI().getPath();
 
-        sendHttpResponse(httpExchange, httpStatusCode);
+        if ("GET".equals(method) && "/tasks".equals(path)) {
+            sendHttpResponse(httpExchange, 200);
+        }
+
+        if ("GET".equals(method) && isTasksPathWithId(path)) {
+            sendHttpResponse(httpExchange, 200);
+        }
+
+        if ("POST".equals(method) && "/tasks".equals(path)) {
+            sendHttpResponse(httpExchange, 201);
+        }
+
+        if (("PATCH".equals(method) || "PUT".equals(method)) && isTasksPathWithId(path)) {
+            sendHttpResponse(httpExchange, 200);
+        }
+
+        if ("DELETE".equals(method) && isTasksPathWithId(path)) {
+            sendHttpResponse(httpExchange, 204);
+        }
+
+        sendHttpResponse(httpExchange, 404);
+    }
+
+    private boolean isTasksPathWithId(String path) {
+        return Pattern.matches("/tasks/[0-9]*$", path);
     }
 
     private void sendHttpResponse(HttpExchange httpExchange, int httpStatusCode)
         throws IOException {
+        sendHttpResponse(httpExchange, httpStatusCode, "{}");
+    }
+
+    private void sendHttpResponse(HttpExchange httpExchange, int httpStatusCode, String message)
+        throws IOException {
         OutputStream outputStream = httpExchange.getResponseBody();
 
         httpExchange.sendResponseHeaders(httpStatusCode, 0);
-
-        String message = "abc";
 
         outputStream.write(message.getBytes());
         outputStream.close();
