287f349f2653cdaef4ef5df277ac347c8ffcda4f
pangnem <sprnd645@gmail.com>
2021-09-23 14:09:40 +0900

    feat: 할 일 객체 추가

---
+++ app/src/main/java/com/codesoom/assignment/Task.java
@@ -0,0 +1,12 @@
+package com.codesoom.assignment;
+
+public class Task {
+
+    private final Long id;
+    private final String title;
+
+    public Task(Long id, String title) {
+        this.id = id;
+        this.title = title;
+    }
+}
