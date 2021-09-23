8c7d1c6dd54cbc3f8b107652ad67f6e83055c369
pangnem <sprnd645@gmail.com>
2021-09-23 11:25:05 +0900

    feat: Http 서버 생성 및 시작

--- app/src/main/java/com/codesoom/assignment/App.java
+++ app/src/main/java/com/codesoom/assignment/App.java
@@ -1,10 +1,25 @@
 package com.codesoom.assignment;
 
+import com.sun.net.httpserver.HttpServer;
+import java.io.IOException;
+import java.net.InetSocketAddress;
+
 public class App {
-    public String getGreeting() {
-        return "Hello World!";
+
+    private static final int PORT = 8000;
+    private static final int BACKLOG = 0;
+
+    public static void main(String[] args) throws IOException {
+        HttpServer httpServer = HttpServer.create(new InetSocketAddress(PORT), BACKLOG);
+
+        httpServer.createContext("/tasks");
+
+        httpServer.start();
+
+        System.out.printf("Server Started. Port: %s", PORT);
     }
 
-    public static void main(String[] args) {
+    public String getGreeting() {
+        return "Hello World!";
     }
 }
